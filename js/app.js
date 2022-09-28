c32230369cc2911f84af379b8f8c7ca13f05d360
DiegoVelascoGH <20193tn054@utez.edu.mx>
2022-09-27 22:38:19 -0500

    [ADD] Adding the activity 4

---
+++ js/app.js
@@ -0,0 +1,28 @@
+if(navigator.serviceWorker){
+    navigator.serviceWorker.register('/sw.js');
+
+    // sumar = () => {
+    //     caches.match('cache-v1').then((response)=>{
+    //         console.log(response);
+    //         //window.location.href = response.text;
+    //     })
+    // }
+    
+    // restar = () => {
+    //     caches.match('cache-v1').then((response)=>{
+    //         //window.location.href = response.text;
+    //     })
+    // }
+    
+    // dividir = () => {
+    //     caches.match('cache-v1').then((response)=>{
+    //         //window.location.href = response.text;
+    //     })
+    // }
+    
+    // multiplicar = () => {
+    //     caches.match('cache-v1').then((response)=>{
+    //         //window.location.href = response.text;
+    //     })
+    // }
+}
---
+++ sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', (event) => {
+    console.log('SW: Instalado');
+    const promiseCache = caches.open('cache-v1').then((cache) => {
+        //addAll es una promesa
+        return cache.addAll(
+            [
+                '/',
+                '/index.html',
+                '/js/app.js',
+                '/pages/suma.html',
+                '/pages/resta.html',
+                '/pages/division.html',
+                '/pages/multiplicacion.html',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js'
+            ]
+        );
+    });
+    event.waitUntil(promiseCache);
+})
+
+
+self.addEventListener('fetch', (event) => {
+    const respCache = caches.match(event.request)
+    event.respondWith(respCache);
+})
