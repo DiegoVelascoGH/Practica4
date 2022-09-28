c855d417043713562ae6f411cd4ca90e1662b4bc
DiegoVelascoGH <20193tn054@utez.edu.mx>
2022-09-27 22:58:19 -0500

    [UPDATE] Fixing service worker

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('./sw.js');
 
     // sumar = () => {
     //     caches.match('cache-v1').then((response)=>{
